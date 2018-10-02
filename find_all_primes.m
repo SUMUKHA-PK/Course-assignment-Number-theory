7dd632b7e49035ffc12de61beb12d4438e116769
Sumukha Pk <sumukhapk46@gmail.com>
2018-10-02 12:15:27 +0530

    Almost done, final touches, comments, testing needed

--- find_all_primes.m
+++ find_all_primes.m
@@ -1,1 +1,15 @@
-% @ Sumukha code this
+% Generates numbers up to given range and provides all  primes satisying
+% the criteria
+
+function array = find_all_primes(a,b,primes_range)
+   array=[];
+    for i = 1:primes_range
+        %disp(i);
+       if is_prime(i) && mod(i,b) == mod(a,b)
+           disp(i);
+           array = [ array ; i];
+       end
+   end
+   disp(array);
+   return
+end
